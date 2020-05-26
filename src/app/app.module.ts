093cdf8e3bdf89d3379860383daa6a2e0de77c61
renatomarcio18081971 <renato.marcio@gmail.com>
2020-05-26 18:27:43 -0300

    topo incluido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TopoComponent } from './Topo';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule,
